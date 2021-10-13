ce291223a2e0b06219932a9ef611247c17889d79
syaku <syaku@naver.com>
2021-10-13 22:07:24 +0900

    account 모듈 분리 작업

--- authorization-server/src/test/java/io/github/syakuis/oauth2/authorization/token/application/AccountUserDetailsServiceTest.java
+++ authorization-server/src/test/java/io/github/syakuis/oauth2/authorization/token/application/AccountUserDetailsServiceTest.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * @author Seok Kyun. Choi.
@@ -18,6 +22,8 @@ class AccountUserDetailsServiceTest {
 
     @Test
     void loadUserByUsername() {
-        accountUserDetailsService.loadUserByUsername("test");
+        UserDetails userDetails = accountUserDetailsService.loadUserByUsername("test");
+        assertNotNull(userDetails);
+        assertEquals("test", userDetails.getUsername());
     }
 }
