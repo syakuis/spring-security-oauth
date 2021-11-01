a018f7e56b81fe24800c97f54c2c4450b995d83a
syaku <syaku@naver.com>
2021-11-01 17:07:18 +0900

    redis token store 변경 작업중

--- src/main/java/io/github/syakuis/oauth2/authorization/security/endpoint/CheckTokenHandleInterceptor.java
+++ src/main/java/io/github/syakuis/oauth2/authorization/security/endpoint/CheckTokenHandleInterceptor.java
@@ -1,10 +1,16 @@
 package io.github.syakuis.oauth2.authorization.security.endpoint;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.JdkSerializationStrategy;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStoreSerializationStrategy;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -17,15 +23,21 @@ import javax.servlet.http.HttpServletResponse;
  */
 @Slf4j
 public class CheckTokenHandleInterceptor implements HandlerInterceptor {
+    private static final String AUTH_TO_ACCESS = "auth_to_access:";
     private final TokenStore tokenStore;
+    private final RedisTemplate redisTemplate;
 
-    public CheckTokenHandleInterceptor(TokenStore tokenStore) {
+    private RedisTokenStoreSerializationStrategy serializationStrategy = new JdkSerializationStrategy();
+
+    public CheckTokenHandleInterceptor(RedisTemplate redisTemplate, TokenStore tokenStore) {
+        this.redisTemplate = redisTemplate;
         this.tokenStore = tokenStore;
     }
 
     private boolean isTarget(HttpServletRequest request) {
         return new AntPathRequestMatcher("/oauth/check_token", HttpMethod.POST.name()).matches(request);
     }
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if (!isTarget(request)) {
@@ -35,10 +47,13 @@ public class CheckTokenHandleInterceptor implements HandlerInterceptor {
         String token = request.getParameter("token");
         log.debug("====> {}", token);
 
-        OAuth2Authentication authentication = tokenStore.readAuthentication(token);
-        log.debug("====> {}", authentication);
-        OAuth2AccessToken accessToken = tokenStore.getAccessToken(authentication);
+        final ValueOperations<byte[], byte[]> valueOpt = redisTemplate.opsForValue();
+
+        byte[] key = serializationStrategy.serialize(AUTH_TO_ACCESS + token);
+        byte[] value = valueOpt.get(key);
+        OAuth2AccessToken accessToken = serializationStrategy.deserialize(value, OAuth2AccessToken.class);
         log.debug("====> {}", accessToken);
+        request.setAttribute("token", accessToken.getValue());
         return true;
     }
 }
