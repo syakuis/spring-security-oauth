a0919a0ba3480e525094c769d99cb4e50866050d
syaku <syaku@naver.com>
2022-06-28 22:10:27 +0900

    설정 추가 및 테스트 코드 추가

--- src/main/java/io/github/syakuis/oauth2/configuration/WebMvcConfiguration.java
+++ src/main/java/io/github/syakuis/oauth2/configuration/WebMvcConfiguration.java
@@ -1,20 +1,26 @@
 package io.github.syakuis.oauth2.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
 @Configuration
-public class WebMvcConfiguration implements WebMvcConfigurer {
+public class WebMvcConfiguration extends DelegatingWebMvcConfiguration {
     private final ObjectMapper objectMapper;
 
     @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        StringHttpMessageConverter stringConverter = new StringHttpMessageConverter(StandardCharsets.UTF_8);
+        converters.add(stringConverter);
         converters.add(new MappingJackson2HttpMessageConverter(objectMapper));
+        super.addDefaultHttpMessageConverters(converters);
     }
 }
